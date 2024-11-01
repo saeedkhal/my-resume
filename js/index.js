fb4dcd0c877a3d1da2b234495ce50220209d9c02
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2024-11-02 00:48:18 +0200

    update

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const job = 'Full Stack Developer';
-// const job = 'Front End Developer';
+// const job = 'Full Stack Developer';
+const job = 'Front End Developer';
 
 const isFrontEndOnly = job.toLocaleLowerCase().includes('front')
